914c68180452796fe0b5c25db595755a36a38ff3
Timothy Robinson <timmyrobby@gmail.com>
2017-06-13 17:16:34 +0100

    Creates basic grid for gameplay

---
+++ game.rb
@@ -0,0 +1,18 @@
+class Game
+  attr_reader :grid
+
+  def initialize
+    @grid = [
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0],
+    ]
+  end
+
+  def choose_starting_square(row, column)
+    @grid[row][column] = 1
+  end
+end
---
+++ spec/game_spec.rb
@@ -0,0 +1,24 @@
+require_relative '../game'
+
+describe Game do
+  let(:game) { described_class.new }
+
+  it { is_expected.to respond_to :grid }
+  it { is_expected.to respond_to :choose_starting_square }
+
+  describe '#choose_starting_square' do
+    it 'updates selected square with given co-ordinates' do
+      game.choose_starting_square(3, 1)
+      expect(game.grid).to eq (
+        [
+          [0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0],
+          [0, 1, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0],
+        ]
+      )
+    end
+  end
+end
