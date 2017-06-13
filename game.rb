c98d8ce91516bf96675f862deaa468701b43afca
Timothy Robinson <timmyrobby@gmail.com>
2017-06-13 18:44:48 +0100

    Privatises game methods

--- game.rb
+++ game.rb
@@ -17,10 +17,31 @@ class Game
     @grid[row][column] = 1
   end
 
+  def start
+    @grid.each_with_index do |row, row_index|
+      row.each_with_index do |cell, cell_index|
+        update_cell(row_index, cell_index)
+      end
+    end
+  end
+
+  private
+
   def update_cell(row, cell)
     neighbours = find_all_neighbours(row, cell)
+    if neighbours.count(1).between?(2, 3)
+      create_cell(row, cell)
+    else
+      destroy_cell(row, cell)
+    end
+  end
 
+  def destroy_cell(row, cell)
+    @grid[row][cell] = 0
+  end
 
+  def create_cell(row, cell)
+    @grid[row][cell] = 1
   end
 
   def find_all_neighbours(row, cell)
@@ -41,13 +62,5 @@ class Game
   def neighbour(row, cell)
     return 0 unless @grid[row]
     @grid[row][cell]
-  end
-
-  def start
-    @grid.each_with_index do |row, row_index|
-      row.each_with_index do |cell, cell_index|
-        p update_cell(row_index, cell_index)
-      end
-    end
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -33,18 +33,18 @@ describe Game do
     end
   end
 
-  describe '#neighbour' do
-    it 'returns the specified cell content' do
-      game.choose_starting_square(1, 3)
-      expect(game.neighbour(1, 3)).to eq 1
-    end
-  end
-
-  describe '#update_cell' do
-    it 'will determine living/dead status'
-  end
-
   describe '#start' do
-    it 'destroys a lone square'
+    it 'destroys a lone square' do
+      game.choose_starting_square(3, 1)
+      game.start
+      expect(game.grid).to eq [
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0]
+      ]
+    end
   end
 end
