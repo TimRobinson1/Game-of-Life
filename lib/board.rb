f94a24ba905b7c04866435fca5d6537b4daae84b
Timothy Robinson <timmyrobby@gmail.com>
2017-06-14 12:53:59 +0100

    Board now updates cells

--- lib/board.rb
+++ lib/board.rb
@@ -9,4 +9,49 @@ class Board
     @grid[row][column] = 1
   end
 
+  def tick
+    @grid.each_with_index do |row, row_index|
+      row.each_with_index do |cell, cell_index|
+        update_cell(row_index, cell_index)
+      end
+    end
+  end
+
+  def update_cell(row, cell)
+    neighbours = find_all_neighbours(row, cell)
+    if neighbours.count(1).between?(2, 3)
+      create_cell(row, cell)
+    else
+      destroy_cell(row, cell)
+    end
+  end
+
+  def destroy_cell(row, cell)
+    @grid[row][cell] = 0
+  end
+
+  def create_cell(row, cell)
+    @grid[row][cell] = 1
+  end
+
+  def find_all_neighbours(row, cell)
+    [
+      neighbour(row - 1, cell - 1),
+      neighbour(row - 1, cell),
+      neighbour(row - 1, cell + 1),
+
+      neighbour(row, cell - 1),
+      neighbour(row, cell + 1),
+
+      neighbour(row + 1, cell - 1),
+      neighbour(row + 1, cell),
+      neighbour(row + 1, cell + 1)
+    ]
+  end
+
+  def neighbour(row, cell)
+    return 0 unless @grid[row]
+    @grid[row][cell]
+  end
+
 end
--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,5 @@
+require_relative 'board'
+
 # Game logic
 class Game
   attr_reader :board
@@ -15,49 +17,6 @@ class Game
   end
 
   def start
-    @board.each_with_index do |row, row_index|
-      row.each_with_index do |cell, cell_index|
-        update_cell(row_index, cell_index)
-      end
-    end
-  end
-
-  private
-
-  def update_cell(row, cell)
-    neighbours = find_all_neighbours(row, cell)
-    if neighbours.count(1).between?(2, 3)
-      create_cell(row, cell)
-    else
-      destroy_cell(row, cell)
-    end
-  end
-
-  def destroy_cell(row, cell)
-    @board[row][cell] = 0
-  end
-
-  def create_cell(row, cell)
-    @board[row][cell] = 1
-  end
-
-  def find_all_neighbours(row, cell)
-    [
-      neighbour(row - 1, cell - 1),
-      neighbour(row - 1, cell),
-      neighbour(row - 1, cell + 1),
-
-      neighbour(row, cell - 1),
-      neighbour(row, cell + 1),
-
-      neighbour(row + 1, cell - 1),
-      neighbour(row + 1, cell),
-      neighbour(row + 1, cell + 1)
-    ]
-  end
-
-  def neighbour(row, cell)
-    return 0 unless @board[row]
-    @board[row][cell]
+    @board.tick
   end
 end
